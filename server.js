7ef952ddc89a44ee92a1b39f2153912c1436c054
blvckroby <scatoroby@gmail.com>
2026-04-29 17:07:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const { serveHTTP } = require("stremio-addon-sdk");
 const addonInterface = require("./addon");
 
-// Render e altri servizi cloud passano la porta tramite una variabile d'ambiente
-const port = process.env.PORT || 7000; 
+// Hugging Face usa la porta 7860 di default
+const port = process.env.PORT || 7860;
 
 serveHTTP(addonInterface, { port });
-console.log(`Addon online sulla porta ${port}`);
+console.log(`Addon avviato sulla porta ${port}`);
